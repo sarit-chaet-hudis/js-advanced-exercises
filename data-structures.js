821cd73152cd682abc321b9cb6f8b763472ba39d
sarit-chaet-hudis <sarit.chh@gmail.com>
2021-11-22 11:42:46 +0200

    implement linked list

--- data-structures.js
+++ data-structures.js
@@ -23,7 +23,7 @@ const stackObj = {
       this.stack.push(element);
   },
   pop: function () {
-      
+
       return this.stack.pop();
   }
 };
@@ -33,3 +33,28 @@ console.log("stack after pushing purple: ",stackObj.stack);
 console.log("pop last element that was pushed: ",stackObj.pop());
 console.log("stack after popping: ",stackObj.stack);
 
+// Implement linked list
+
+class LinkedList {
+    constructor(head = null) {
+        this.head = head
+    }
+}
+
+class ListNode {
+    constructor(data) {
+        this.data = data
+        this.next = null
+    }
+}
+
+let node1 = new ListNode("content of node 1");
+let node2 = new ListNode("hi im the second node");
+// now link between node 1 and node 2 :
+node1.next = node2;
+
+// Lets create a linked list with node1 as its head!
+let LinkedList1 = new LinkedList(node1);
+
+// now access the second node with our linked list:
+console.log(LinkedList1.head.next.data);
