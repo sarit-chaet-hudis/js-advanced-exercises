d568a8443597796c3126bd996c28e00d0692d581
sarit-chaet-hudis <sarit.chh@gmail.com>
2021-11-22 13:59:43 +0200

    comment

--- exponent.js
+++ exponent.js
@@ -3,7 +3,7 @@ function findExponent(base, num) {
         return 1;
     }
     
-    return findExponent(base, num/base) + 1;
+    return findExponent(base, num/base) + 1; //the plus one counts how many times the recursion has happened
 }
 
 console.log(findExponent(8,64));
