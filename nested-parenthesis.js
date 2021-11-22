0664cbcca03c3bef00642d85fbc88ba60a80f7ef
sarit-chaet-hudis <sarit.chh@gmail.com>
2021-11-22 13:55:11 +0200

    nested parenthesis check using stack, different types of parenthesis

---
+++ nested-parenthesis.js
@@ -0,0 +1,35 @@
+// properly nested if any of the following conditions is true:
+// S is empty;
+// S has the form "(U)" or "[U]" or "{U}" where U is a properly nested string;
+// S has the form "VW" where V and W are properly nested strings.
+// For example, the string "{[()()]}" is properly nested but "([)()]" is not.
+//  returns 1 if S is properly nested and 0 otherwise.
+
+function solution(S) {
+  if (S === "") return 1;
+  let bracketStack = [];
+  for (let char of S) {
+    if (char === "{" || char === "[" || char === "(") {
+      bracketStack.push(char);
+    } else {
+      let lastChar = bracketStack.pop();
+      switch (char) {
+        case ")":
+          if (lastChar != "(") return 0;
+          break;
+        case "]":
+          if (lastChar != "[") return 0;
+          break;
+        case "}":
+          if (lastChar != "{") return 0;
+          break;
+      }
+    }
+  }
+  return bracketStack.length === 0 ? 1 : 0;
+}
+
+const s1 = "{[()()]}";
+const s2 = "{{{{";
+
+console.log(solution(s2));
