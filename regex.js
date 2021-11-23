83718241069645cf4e271447aeef265c5f258414
sarit-chaet-hudis <sarit.chh@gmail.com>
2021-11-23 11:37:47 +0200

    regex

---
+++ regex.js
@@ -0,0 +1,5 @@
+// The .test() method takes the regex, applies it to a string (which is placed inside the parentheses), and returns true or false if your pattern finds something or not.
+
+let testStr = "freeCodeCamp";
+let testRegex = /Code/;
+console.log(testRegex.test(testStr)); //true
