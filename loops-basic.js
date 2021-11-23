5dcf80ec084ba55b563f9b943451b424912abff3
sarit-chaet-hudis <sarit.chh@gmail.com>
2021-11-23 13:38:06 +0200

    loop basics

---
+++ loops-basic.js
@@ -0,0 +1,11 @@
+// print all nums from 100 to -4 div by 5
+
+// for (let i = 100; i > -4; i = i-5) {
+//     console.log(i);
+// }
+
+// isolate digits 
+let x = 123;
+console.log(Math.floor(x/100)); // 1
+console.log(Math.floor(x/10%10)); //2
+console.log(Math.floor(x%10)); //3
